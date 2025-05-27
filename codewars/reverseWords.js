f9338ca81e7baf378717b96d1378834bb9d80a88
Cimi-nex <ciminolucas.carvalho@gmail.com>
2025-05-26 23:28:05 -0300

    primeiro teste do codewars

---
+++ codewars/reverseWords.js
@@ -0,0 +1,11 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// Complete a função que aceita um parâmetro string e inverte cada palavra na string. Todos os espaços na string devem ser mantidos.
+
+// examples:
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+
+function reverseWords(str) {
+  // Go for it
+}
